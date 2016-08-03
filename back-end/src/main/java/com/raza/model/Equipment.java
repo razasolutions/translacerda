60a7a77473ee5bc9f6e305262e0f6350d230e801
henriqsg <henriqsg@hotmail.com>
2016-08-02 21:38:24 -0300

    Equipment Class Created.

---
+++ back-end/src/main/java/com/raza/model/Equipment.java
@@ -0,0 +1,17 @@
+package com.raza.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class Equipment extends BaseEntity(Long)  {
+	
+	private static final long serialVersionUID = 6025566720560241474L;
+	
+	@Column
+	private String licensePlate;
+	private String model;
+	private String name;
+	private String color;
+	
+}
