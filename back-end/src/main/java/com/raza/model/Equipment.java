0811d3ff1b43227628ddc7641d3d66656bd5a25f
henriqsg <henriqsg@hotmail.com>
2016-08-02 22:00:52 -0300

    Equipment Getters and Setters created.

--- back-end/src/main/java/com/raza/model/Equipment.java
+++ back-end/src/main/java/com/raza/model/Equipment.java
@@ -6,12 +6,47 @@ import javax.persistence.Entity;
 @Entity
 public class Equipment extends BaseEntity<Long>  {
 	
-	private static final long serialVersionUID = 6025566720560241474L;
+	private static final long serialVersionUID = -8506028513793354501L;
 	
-	@Column
+	@Column							/** deve ter um jeito mais fácil de se declarar **/
 	private String licensePlate;
+	@Column
 	private String model;
+	@Column
 	private String name;
+	@Column
 	private String color;
+	
+	/** Getters and Setters **/
+	
+	public String getLicensePlate() {
+		return licensePlate;
+	}
+	public void setLicensePlate(String licensePlate) {
+		this.licensePlate = licensePlate;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
 	
 }
