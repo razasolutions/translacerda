a8a8f7b10885686317d3bda313b783e493843324
chtnunes <chtnunes@gmail.com>
2016-08-12 22:32:14 -0300

    Switching "()" to "<>" to make the RegisterRepository routine be ok.

--- back-end/src/main/java/com/raza/repositories/RegisterRepository.java
+++ back-end/src/main/java/com/raza/repositories/RegisterRepository.java
@@ -2,7 +2,9 @@ package com.raza.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RegisterRepository extends JpaRepository(Register, Long){
+import com.raza.model.Register;
+
+public interface RegisterRepository extends JpaRepository<Register, Long>{
 	
 	public Register findById(Long id);
 
