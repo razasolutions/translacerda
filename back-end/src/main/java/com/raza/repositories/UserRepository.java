d884a559690e664871d8e7afe4fab4496de6082c
henriqsg <henriqsg@hotmail.com>
2016-08-17 14:56:16 -0300

    User repository OK.

--- back-end/src/main/java/com/raza/repositories/UserRepository.java
+++ back-end/src/main/java/com/raza/repositories/UserRepository.java
@@ -8,13 +8,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 	public User findById(Long id);
 	
-	/* Não precisariam estar aqui:
-	
-	public List<User> findAll();
-	 
-	public User save(User user);
-	
-	public void delete(User user);
-	
-	*/
 }
